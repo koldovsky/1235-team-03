feaa98ab4b37f340180de9fd76f543ffe313cd1b
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2024-07-18 11:04:11 +0000

    Minor fixes in header-nav

--- js/header-nav.js
+++ js/header-nav.js
@@ -1,13 +1,14 @@
 const body = document.querySelector("body");
 const headerNav = document.querySelector(".header__navigation");
+const headerLinks = document.querySelector(".header-links__link-item");
 const navBurgerBtn = document.querySelector(".header-nav__burger");
 
 navBurgerBtn.addEventListener("click", () => {
+    body.classList.toggle("overflow-hidden");
     headerNav.classList.toggle("header-nav--burger-active");
 });
 
-headerNav.addEventListener("wheel", (e) => {
-    if (headerNav.classList.contains("header-nav--burger-active")) {
-        e.preventDefault();
-    }
+headerLinks.addEventListener("click", () => {
+    body.classList.remove("overflow-hidden");
+    headerNav.classList.remove("header-nav--burger-active");
 });
