3722ead32053d670e5379662365be4a3bb8d86da
Natalia Zaretska <nzaretska@gmail.com>
2024-07-19 11:36:09 +0000

    messi-goals.js

---
+++ codewars/messi-goals.js
@@ -0,0 +1,4 @@
+//Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
