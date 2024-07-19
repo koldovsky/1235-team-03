1f0db1561324689f446d73fd1db00c8f744429a8
Natalia Zaretska <nzaretska@gmail.com>
2024-07-19 14:26:12 +0200

    Update return-negative.js

--- codewars/return-negative.js
+++ codewars/return-negative.js
@@ -9,7 +9,7 @@ makeNegative(0.12); // return -0.12
 Notes
 The number can be negative already, in which case no change is required.
 Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense. */
-//Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
 function makeNegative(num) {
   return num > 0 ? 0 - num : num;
 }
