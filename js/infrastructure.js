dd2e0b5f77287d9a22a99367ac1fda94f515be55
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-07-22 16:33:34 +0000

    Add JavaScript

--- js/infrastructure.js
+++ js/infrastructure.js
@@ -1,3 +1,17 @@
-
+function init() {
+    import('./header-nav.js');
+  
+    const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+    let loadedPartialsCount = 0;
+  
+    document.body.addEventListener('htmx:afterOnLoad', () => {
+      loadedPartialsCount++;
+      if (loadedPartialsCount === totalPartials) {
+        init();
+      }
+    });
+  }
+  
+  init();  
   
   
