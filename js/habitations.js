fffaf9d0af2c9106a8b9effd6ad64f5c8e2d470b
bewayos <165403463+bewayos@users.noreply.github.com>
2024-07-26 09:00:22 +0000

    create habitations.cards.js and habitations.special-offers.js

--- js/habitations.js
+++ js/habitations.js
@@ -1,5 +1,7 @@
 function init() {
     import("./header-nav.js");
+    import("./habitations.card.js");
+    import("./habitations.special-offers.js");
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
