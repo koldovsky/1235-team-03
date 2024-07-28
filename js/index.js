bead7979bc9893fc4f0fdcc2b113664e94ab7176
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-07-28 17:04:58 +0000

    Add css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function init() {
     import("./header-nav.js");
     import("./habitations.js");
+    import("./infrastructure.js");
   }
   
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
