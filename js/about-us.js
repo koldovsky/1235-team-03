d6d1b1cad3e1695cd02f590c3bef328021dc3786
olesia-wq <olesiay170@gmail.com>
2024-07-31 17:13:08 +0000

    restart css

--- js/about-us.js
+++ js/about-us.js
@@ -12,16 +12,3 @@ document.body.addEventListener('htmx:afterOnLoad', () => {
     }
 });
 
-document.querySelectorAll('.accordion-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const accordionContent = button.nextElementSibling;
-
-        button.classList.toggle('active');
-
-        if (button.classList.contains('active')) {
-            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-        } else {
-            accordionContent.style.maxHeight = 0;
-        }
-    });
-});
